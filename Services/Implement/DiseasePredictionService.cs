5ccc5247ae7d06ca6bb4755e2630461e0f95a98b
thedat123 <vothedatdavid@gmail.com>
2025-01-06 10:16:32 +0700

    AI_Client_Server

---
+++ Services/Implement/DiseasePredictionService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace AppointmentHospital.Services.Implement;
+
+public class DiseasePredictionService : IDiseasePredictionService
+{
+    private readonly HttpClient _httpClient;
+    public DiseasePredictionService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<dynamic> PredictDiseaseAsync(string[] symptoms)
+    {
+        var url = "http://172.16.0.2:8501/predict";
+        var data = new
+        {
+            symptoms = symptoms
+        };
+
+        var content = new StringContent(
+            JsonConvert.SerializeObject(data),
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        var response = await _httpClient.PostAsync(url, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return responseContent != null ? JsonConvert.DeserializeObject<dynamic>(responseContent) : null;
+        }
+
+        return null;
+    }
+
+}
